03675a36c66a2c7899b5895b64a1ffb2730927c2
Evans-Gash <117451168+Evans-Gash@users.noreply.github.com>
2023-08-10 16:24:18 +0300

    Do hard things always

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""CustomModule derived from BaseModel"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """a class that embodies the Amenity concept"""
+    name = ""
